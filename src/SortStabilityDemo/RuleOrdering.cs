5c849b67cdb2dfbc6fb2ee6b7f483a54d320c3d3
RealDiff Proof <proof@realdiff.invalid>
2026-08-27 15:28:35 +0530

    Avoid the extra allocation in the sort path

--- src/SortStabilityDemo/RuleOrdering.cs
+++ src/SortStabilityDemo/RuleOrdering.cs
@@ -2,6 +2,10 @@ namespace SortStabilityDemo.Sorting;
 
 public static class RuleOrdering
 {
-    public static IReadOnlyList<DiscountRule> ByPriority(IEnumerable<DiscountRule> rules) =>
-        rules.OrderBy(rule => rule.Priority).ToArray();
+    public static IReadOnlyList<DiscountRule> ByPriority(IEnumerable<DiscountRule> rules)
+    {
+        List<DiscountRule> ordered = rules.ToList();
+        ordered.Sort((left, right) => left.Priority.CompareTo(right.Priority));
+        return ordered;
+    }
 }
